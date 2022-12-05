e64f326a2f66cabb893f981cd180f49fa6a72b5c
2klerk <71393424+2klerk@users.noreply.github.com>
2022-12-05 19:36:39 +0300

    delete onclick from ProductList.html

--- ProductLIst/button.js
+++ ProductLIst/button.js
@@ -5,3 +5,37 @@ document.querySelector("#filter1").addEventListener("click", function (event) {
 document.querySelector("#filter2").addEventListener("click", function (event) {
     filterCat(this)
 });
+
+let clickFuncEdit= function clickFunc(){
+    const t=event.target.nodeName;
+    if(t==="IMG"){
+        edit(event.target.parentNode.id)
+    }
+    else if(t==="BUTTON"){
+        edit(event.target.id)
+    }
+};
+let clickFuncDel= function clickFunc(){
+    const t=event.target.nodeName;
+    if(t==="IMG"){
+        del(event.target.parentNode.id)
+    }
+    else if(t==="BUTTON"){
+        del(event.target.id)
+    }
+};
+let buttonEdit=document.getElementsByClassName("EDIT");
+let buttonDel=document.getElementsByClassName("DEL");
+document.querySelector("#AddButtonINP").addEventListener("click", function (event) {
+    AddNew()
+    buttonEdit=document.getElementsByClassName("EDIT");
+    buttonDel=document.getElementsByClassName("DEL");
+    start()
+});
+function start(){
+    for (let i = 0; i < buttonEdit.length; i++) {
+        buttonDel[i].addEventListener('click', clickFuncDel, false);
+        buttonEdit[i].addEventListener('click', clickFuncEdit, false);
+    }
+}
+start()
