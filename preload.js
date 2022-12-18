133a21c952b6bbfce69aea603370d4c9157b30a5
2klerk <71393424+2klerk@users.noreply.github.com>
2022-12-18 21:09:55 +0300

    fixed preload.js

--- preload.js
+++ preload.js
@@ -1,22 +1,27 @@
 function preload(){
+    const html=document.querySelector("html")
     const body=document.querySelector("body")
     const img=document.createElement('img')
     console.log("preload");
-    document.querySelector("html").style.pointerEvents="none"
-    document.querySelector("body").style.opacity = "0%";
+    html.style.display="flex";
+    html.style.justifyContent="center";
+    html.style.overflow="hidden";
+    html.style.pointerEvents="none"
+    body.style.opacity = "0%";
     img.src="img/preload2.gif"
-    document.querySelector("html").appendChild(img);
+    html.appendChild(img);
     img.style.position="absolute"
-    img.style.top="50%";
-    img.style.left="45%";
+    img.style.top="50%"
     img.style.width="100px"
     img.style.height="100px"
     setTimeout(() => {
+        html.style.overflow="visible"
         body.style.opacity = "100%";
+        html.style.display="block";
         img.remove()
     }, 700);
     setTimeout(() => {
-        document.querySelector("html").style.pointerEvents="fill"
+        html.style.pointerEvents="fill"
     }, 100);
 
 }
